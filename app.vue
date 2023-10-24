8e851b0c193096f9f2ca9de7ba981abd27254f69
fredrickrogath <fredle560@gmail.com>
2023-10-24 04:07:58 +0300

    Configurations

--- app.vue
+++ app.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="dark">
+  <el-button @click="ElMessage('hello')">button</el-button>
+  <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+  <LazyElButton type="warning">lazy button</LazyElButton>
+
+  
+{{isDark}}
+  <el-switch
+    v-model="isDark"
+    :before-change="notifyDarkChanged"
+    :active-action-icon="View"
+    :inactive-action-icon="Hide"
+     loading
+  />
   </div>
 </template>
+
+<script setup>
+import { ElMessage } from 'element-plus'
+import { useDark, useToggle } from '@vueuse/core'
+import { Hide, View } from '@element-plus/icons-vue'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+const notifyDarkChanged = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      ElMessage.success(!isDark.value ? 'Dark Mode' : 'Light Mode')
+      return resolve(true)
+    }, 1)
+  })
+}
+
+
+</script>

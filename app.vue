2b0536a1fd881f08e65acf3966f96b4ad059216b
fredrickrogath <fredle560@gmail.com>
2023-10-24 13:52:11 +0300

    Configurations

--- app.vue
+++ app.vue
@@ -1,30 +1,35 @@
 <template>
-  <div class="dark">
+  <!-- <div>
   <el-button @click="ElMessage('hello')">button</el-button>
   <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
   <LazyElButton type="warning">lazy button</LazyElButton>
 
+  <p class="text-red-700">
+
+    {{isDark}}
+  </p>
   
-{{isDark}}
   <el-switch
     v-model="isDark"
     :before-change="notifyDarkChanged"
-    :active-action-icon="View"
-    :inactive-action-icon="Hide"
-     loading
+    :active-action-icon="Moon"
+    :inactive-action-icon="Sunny"
+    
   />
-  </div>
+  </div> -->
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { ElMessage } from 'element-plus'
 import { useDark, useToggle } from '@vueuse/core'
-import { Hide, View } from '@element-plus/icons-vue'
+import { Moon, Sunny } from '@element-plus/icons-vue'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 
 const notifyDarkChanged = () => {
+  const toggleDark = useToggle(isDark)
   return new Promise((resolve) => {
     setTimeout(() => {
       ElMessage.success(!isDark.value ? 'Dark Mode' : 'Light Mode')
@@ -35,3 +40,11 @@ const notifyDarkChanged = () => {
 
 
 </script>
+
+<style scoped>
+html.dark {
+  /* custom dark bg color */
+  --el-bg-color: #626aef;
+}
+
+</style>
